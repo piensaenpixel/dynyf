ea2e8117e5999f278b16d72c1f411687c9b98a79
Javier Arce <javierarce@gmail.com>
2014-05-30 16:03:28 +0200

    Uses passport

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var store
 sys        = require('util');
 express    = require("express");
 http       = require("http");
+https       = require("https");
 routes     = require("./routes");
 path       = require('path');
 crypto     = require('crypto');
@@ -12,24 +13,33 @@ var morgan       = require('morgan')
 var bodyParser   = require('body-parser');
 var cookieParser = require('cookie-parser');
 
-_         = require("underscore");
-everyauth = require('everyauth');
+_ = require("underscore");
+passport = require('passport')
+FacebookStrategy = require('passport-facebook').Strategy;
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
-var findOrCreateFBUser = function(session, accessToken, accessTokExtra, fbUserMetadata){  
-  return true
-}  
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
-everyauth.facebook
-.appId(process.env.FACEBOOK_APP_ID)
-.appSecret(process.env.FACEBOOK_APP_SECRET)
-.scope('email,user_location,user_photos,publish_actions')
-.handleAuthCallbackError( function (req, res) {
-  res.send('Error occured');
-})
-.findOrCreateUser(findOrCreateFBUser)
-.redirectPath('/callback');
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:5000/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function () {
 
+    console.log(accessToken, refreshToken)
+
+    profile.accessToken  = accessToken;
+
+    return done(null, profile);
+  });
+}));
 
 CartoDB = require('cartodb');
 Config  = require("./lib/config").Config;
@@ -53,7 +63,8 @@ app.use(session({
   secret: 'F;;v,m-{-HC6YqTR}T=;',
   store: store
 }));
-app.use(everyauth.middleware(app));
+ app.use(passport.initialize());
+  app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // CartoDB configuration
@@ -84,22 +95,101 @@ extend = function(origin, add) {
 
 // ROUTES
 // ==========================================
+//
+
+var getOptions = function(host, port, method, path, headers) {
+  return {
+    host: host,
+    port: port,
+    method: method,
+    path: path,
+    headers: _.extend({
+      "Content-Type": "application/json"
+    }, headers)
+  };
+};
+
+var get = function(res, options) {
+  return https.request(options, function(resp) {
+    var data;
+    data = "";
+    resp.on("data", function(chunk) {
+      return data += chunk;
+    });
+    return resp.on("end", function() {
+      res.writeHead(resp.statusCode, {
+        "Content-Type": "application/json"
+      });
+      res.write(data);
+      return res.end();
+    });
+  }).end();
+};
+
+function getMe(req, res) {
+
+  console.log("---")
+  console.log(req.session.passport.user.accessToken)
+  console.log("---")
+
+  var options = getOptions("graph.facebook.com", 443, "GET", "/v2.0/me/events?access_token=" + req.session.passport.user.accessToken);
+
+  return https.request(options, function(resp) {
+    var data;
+    data = "";
+    resp.on("data", function(chunk) {
+      return data += chunk;
+    });
+    return resp.on("end", function() {
+
+      data = JSON.parse(data);
+
+      console.log(data)
+      //for (var i = 0; i< data.data.length; i++) {
+        //console.log(data.data[i].place)
+      //}
+
+      //return res.render("index", {
+      //});
+    });
+  }).end();
+}
 
 app.get("/", function(request, response) {
 
-    console.log(process.env.FACEBOOK_APP_ID)
-    console.log(process.env.FACEBOOK_APP_SECRET)
+    if (request.session.passport) {
+      getMe(request, response)
+    }
 
   return response.render("index");
 });
 
-//app.get("/callback", function(request, response) {
-  //return response.redirect("/");
-//});
-
-app.get("/auth/login", function(request, response) {
-  return response.redirect("/auth/facebook");
+app.get("/callback", function(request, response) {
+  return response.redirect("/");
 });
+
+// GET /auth/facebook
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Facebook authentication will involve
+//   redirecting the user to facebook.com.  After authorization, Facebook will
+//   redirect the user back to this application at /auth/facebook/callback
+app.get('/auth/facebook',
+  passport.authenticate('facebook', { scope: ['read_stream', 'publish_actions'] }),
+  function(req, res){
+    // The request will be redirected to Facebook for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/facebook/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 port = process.env.PORT || Config.port;
 
